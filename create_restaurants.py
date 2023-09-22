0fb4c6723c29064ed48049a68dad541308816f80
AndMWalker <91815643+AndMWalker@users.noreply.github.com>
2023-09-22 17:16:08 +0800

    Add files via upload

--- create_restaurants.py
+++ create_restaurants.py
@@ -16,11 +16,11 @@ class RestaurantDataProcessor:
         self.restaurants_full = pd.DataFrame(columns=self.columns)
         self.country_dict = self.load_country_codes()
 
-    def load_country_codes(self):
+    def load_country_codes(self): #fetch country code data from Country-Code.xlsx
         cc = pd.read_excel('Country-Code.xlsx')
-        return dict(zip(cc['Country Code'], cc['Country']))
+        return dict(zip(cc['Country Code'], cc['Country'])) #make into dictionary for easy access
 
-    def fetch_restaurant_data(self, url):
+    def fetch_restaurant_data(self, url): #fetch restaurant data from url
         response = requests.get(url)
 
         if response.status_code == 200:
@@ -31,20 +31,20 @@ class RestaurantDataProcessor:
         for restaurant_lst in data:
             res = restaurant_lst['restaurants']
             for res_info in res:
-                country_id = int(res_info['restaurant']['location'].get('country_id'))
+                country_id = int(res_info['restaurant']['location'].get('country_id')) #if country code not in Country-Code.xlsx, make country into NA. In this case i will assume not to modify that row any further.
                 country = self.country_dict.get(country_id, 'NA')
                 new_row = {
-                    "Restaurant Id": int(res_info['restaurant']['R'].get('res_id')),
+                    "Restaurant Id": res_info['restaurant']['R'].get('res_id'),
                     "Restaurant Name": res_info['restaurant'].get('name'),
                     "Country": country,
                     "City": res_info['restaurant']['location'].get('city'),
-                    "User Rating Votes": int(res_info['restaurant']['user_rating'].get('votes')),
-                    "User Aggregate Rating (in float)": float(res_info['restaurant']['user_rating'].get('aggregate_rating')),
+                    "User Rating Votes": res_info['restaurant']['user_rating'].get('votes'),
+                    "User Aggregate Rating (in float)": res_info['restaurant']['user_rating'].get('aggregate_rating'),
                     "Cuisines": res_info['restaurant'].get('cuisines')
                 }
                 self.restaurants_full = self.restaurants_full.append(new_row, ignore_index=True)
 
-    def convert_data_types(self):
+    def convert_data_types(self): #These datatypes are assumed (except Rating as that is required)
         data_types = {
             "Restaurant Id": int,
             "Restaurant Name": str,
